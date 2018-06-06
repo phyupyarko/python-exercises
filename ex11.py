b5f34e8b506abb10d1ac723370ac0188438a40cc
phyupyarko <moeeaindray22@gmail.com>
2018-06-06 21:24:14 +0630

    Repair My 11th python ex

--- ex11.py
+++ ex11.py
@@ -1,7 +1,7 @@
-print("How old are you?"),
-age = raw_input()
-print("How tall are you?"),
-height = raw_input()
-print("How much do you weigh?"),
-weight = raw_input()
-print ("So, you're %r old, %r tall and %r heavy." % ( age, height, weight))
+print ("how old are you?",end='')
+age=input()
+print ("how tall are you?",end='')
+height=input()
+print ("how much do you weight?",end='')
+weight=input()
+print(f"so, you're {age}old,{height}tall and {weight} heavy.")
