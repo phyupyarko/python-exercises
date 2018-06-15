f7384dd5bbaeaffca712c2d58e3560b8d21be8f7
phyupyarko <moeeaindray22@gmail.com>
2018-06-15 10:18:09 +0630

    My 40th python ex

---
+++ ex34.py
@@ -0,0 +1,10 @@
+animals = ['bear','python3.6', 'peacock', 'kangaroo', 'whale','platypus']
+i=0
+numbers = []
+
+while i<6:
+    for name in animals:
+    print(f"The first animal is at {i} and is a animals{i}" )
+    numbers.append(i)
+    i = i + 1
+
