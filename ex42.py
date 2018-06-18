316bb626353f0a1a2ab7051d918bbd56c55369fe
phyupyarko <moeeaindray22@gmail.com>
2018-06-18 14:21:55 +0630

    My 43th python ex

--- ex42.py
+++ ex42.py
@@ -1,6 +1,6 @@
 class Animal(object):
     pass 
-class Dog(Animal);
+class Dog(Animal):
 def __init__(self, name):
     self.name = name
 class Cat(Animal):
@@ -15,3 +15,17 @@ class Employee(Person):
     def __init__(self, name, salary):
         super(Employee, self).__init__(name)
         self.salary = slary
+
+class Fish(object):
+    pass
+class Halibut(Fish):
+    pass
+rover = Dog("Rover")
+satan = Cat("Satan")
+mary = Person("Mary")
+mary.pet = satan
+frank = Employee("Frank", 120000)
+frank.pet = rover
+flipper = Fish()
+course = Salmon()
+harry = Halibut()
